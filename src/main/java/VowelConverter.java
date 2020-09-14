4f882e5334325aa6d5d2ffd8b8882391b8ac3d9f
dai-yu <1435480344@qq.com>
2020-09-14 20:36:17 +0800

    feat: test for more than 30 precent discontinuous vowels

--- src/main/java/VowelConverter.java
+++ src/main/java/VowelConverter.java
@@ -14,8 +14,14 @@ public class VowelConverter {
         copyString = copyString.replaceAll("[aeiouAEIOU]", "");
         int vowelsCount = str.length() - copyString.length();
         double vowelsProportion = (float)vowelsCount / str.length();
-
-        return vowelsProportion>0.3?str:str;
+        if(vowelsProportion >0.3) {
+            Pattern pattern = Pattern.compile(CONTINUOUS_VOWELS);
+            Matcher matcher = pattern.matcher(str);
+            while (matcher.find()) {
+                str = str.replaceAll(matcher.group(), String.join(MOMMY, matcher.group().split("")));
+            }
+        }
+        return str;
     }
 
 }
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -14,10 +14,18 @@ public class MommifierTest {
     }
 
     @Test
-    public void should_return_words_itself_when_covert_given_a_string_with_discontinuous_vowels(){
+    public void should_return_words_itself_when_covert_given_a_string_less_than_30_precent_vowels(){
         VowelConverter vc=new VowelConverter();
         String str = "afefif";
         String result = vc.covert(str);
         assertEquals(str, result);
     }
+
+    @Test
+    public void should_return_words_itself_when_covert_given_a_string_with_more_than_30_precent_discontinuous_vowels() {
+        VowelConverter vc = new VowelConverter();
+        String moreThanResult = vc.covert("ikfihof");
+        assertEquals("ikfihof", moreThanResult);
+    }
+
 }
