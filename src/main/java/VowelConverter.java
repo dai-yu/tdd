89f8be3f89321e5addcf3122189f1ad9685f9c7c
dai-yu <1435480344@qq.com>
2020-09-14 20:11:27 +0800

    feat: test for null or empty

---
+++ src/main/java/VowelConverter.java
@@ -0,0 +1,8 @@
+public class VowelConverter {
+    public String convert(String str) {
+        if(str==null || str==""){
+            return str;
+        }
+        return str;
+    }
+}
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -1,2 +1,17 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MommifierTest {
+
+    @Test
+    public void should_return_words_itself_when_convert_given_string_is_null_or_empty(){
+        VowelConverter vc=new VowelConverter();
+        String str=null;
+        String str1="";
+        String nullString=vc.convert(str);
+        String emptyString=vc.convert(str1);
+        assertEquals(nullString, str);
+        assertEquals(emptyString, str1);
+    }
+
+
 }
